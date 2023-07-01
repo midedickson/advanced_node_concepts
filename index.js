14a7b4782b6f7e8525b99adeac8e80152d54e6e7
Mide Dickson <dicksonmide@gmail.com>
2023-07-01 18:20:34 +0100

    clustering

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require("cluster");
 const express = require("express");
+const crypto = require("crypto");
+
 const app = express();
 
 if (cluster.isMaster) {
   cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  //   cluster.fork();
+  //   cluster.fork();
 } else {
   function doWork(duration) {
     const start = Date.now();
@@ -11,8 +19,14 @@ if (cluster.isMaster) {
   }
 
   app.get("/", (req, res) => {
-    doWork(5000);
-    res.send("Hello!");
+    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+      res.send("Hello!");
+    });
+  });
+
+  app.get("/fast", (req, res) => {
+    // doWork(5000);
+    res.send("This was fast!");
   });
 
   app.listen(3000);
